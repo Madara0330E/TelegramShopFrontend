c3e87b4ca09ea3cf0a5d9bdfebec5f51eb0d2b88
Madara0330E <summoners2011sss@gmail.com>
2025-03-25 01:38:01 +0500

    Product category
    Product category

--- src/app/components/CategoryCard.tsx
+++ src/app/components/CategoryCard.tsx
@@ -13,7 +13,7 @@ interface CategoryCardProps {
 
 const CategoryCard: React.FC<CategoryCardProps> = ({ category }) => {
   return (
-    <div className="rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 w-full mb-4">
+    <div className="rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 w-full mb-2">
       <div className="relative w-full aspect-square">
         <img
           src={category.iconUrl}
@@ -25,19 +25,20 @@ const CategoryCard: React.FC<CategoryCardProps> = ({ category }) => {
             style={{
               background: "linear-gradient(90deg, #3C53E8 0%, #7DB3F6 100%)",
             }}
-            className="flex w-full px-2 py-1 justify-center items-center gap-1 absolute bottom-0 rounded-b-lg"
+            className="flex w-full px-2 py-1 h-[7.03125vw] justify-center items-center gap-1 absolute bottom-0 rounded-b-lg"
           >
             <img
               src="img/CategoryCard/SoftStar.svg"
               alt=""
-              className="w-5 h-5"
+              className="w-[3.125vw] h-[3.125vw]"
             />
-            Новое
+            <span className="text-[#EFEDF6]  text-[4.1667vw] text-base font-semibold leading-normal">Новое</span>
+           
           </span>
         )}
       </div>
-      <div className="flex justify-center items-center max-w-[22.14vw]">
-        <p className="text-[#EFEDF6] text-[4vw] text-center w-full break-words whitespace-normal">
+      <div className="flex justify-center items-center mt-0.5 max-w-[22.14vw]">
+        <p className="text-[#EFEDF6] text-[3.6458vw] text-center w-full break-words whitespace-normal">
           {category.name}
         </p>
       </div>
